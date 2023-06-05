bf4ef4dff7654d98a8e2251cdcaea567683610fe
CiubotaruNikita <135308079+CiubotaruNikita@users.noreply.github.com>
2023-06-05 12:06:10 +0300

    Am modifica

--- ex7.py
+++ ex7.py
@@ -1,3 +1,3 @@
 sir = input("Introduceți un șir de caractere: ")
-caracter_specific = sir.lower().count(input("Introduceți caracterul de numărat: "))
-print (caracter_specific)
+caracter = sir.lower().count(input("Introduceți caracterul de numărat: "))
+print (caracter)
